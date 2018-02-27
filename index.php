e145869424ff8223671dc1b14cf252668bec2604
Rachel Do <rachelspdo20@gmail.com>
2018-02-27 15:44:39 -0800

    fix: sql query bug fix

--- index.php
+++ index.php
@@ -52,13 +52,13 @@
                   <?php 
                   // Connect to databse
                     $serverhost = "localhost";
-                    $dbname = 'project001';
+                    $dbname = 'project00';
                     $dbuser = 'root';
                     $dbpassword = '';
 
                     $db = mysqli_connect($serverhost, $dbuser, $dbpassword, $dbname);
                     // Get id and username to display on page
-                    $sql_query = "SELECT * FROM register_account INNER JOIN address ON register_account.id = address.user_id";
+                    $sql_query = "SELECT * FROM register_account INNER JOIN user_address ON register_account.id = user_address.user_id";
                     $result = mysqli_query($db, $sql_query);
                     // looping through all of the accounts and display them
                     while ($row = mysqli_fetch_assoc($result)) {
